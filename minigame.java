4d4bf75b5fa33dc1941ab32b12640da80c3cf8aa
Victoria Arzumanova <44909609+ViccyOctopus@users.noreply.github.com>
2020-09-30 15:00:37 -0400

    Add files via upload

---
+++ minigame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class minigame {
+    public static void main(final String[] args) {
+        System.out.println("This will start the minigame.\n");
+
+        // variables
+
+        // input
+        final Scanner scan = new Scanner(System.in);
+
+        System.out.println("Do you choose to go right or left?");
+        String directions = scan.nextLine();
+        if (directions.equals("right")) {
+            System.out.println("So you go right.\n");
+        }
+        else {
+            System.out.println("So you go left.");
+        }
+
+
+    }
+}
