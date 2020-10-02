1c3af4d2fc35487afbcf1878c7cd2d781d67c6ce
Victoria Arzumanova <victoriaarzumanova123@gmail.com>
2020-10-02 17:05:28 -0400

    added * instead of X because, im not sure

--- Shapes/Shapes.java
+++ Shapes/Shapes.java
@@ -42,6 +42,41 @@ public class Shapes {
         if (unknownShape.equals(boxShape)) {
             System.out.println("You have decided the Box Shape with " + numInRows + " rows and " + numInColumns + " columns");
 
+            String[][] array = new String[numInRows][numInColumns];
+
+            // blank slate
+            for (int i = 0; (i < numInRows); i++) {
+                for (int k = 0; k < numInColumns; k++) {
+                 array[i][k] = " ";
+               }
+            }
+            
+            // This code will place an X at the first column.
+            for (int i = 0; i < numInRows; i++) {
+                array[i][0] = "*";
+            }
+
+            // This code places an X in the first row.
+            for (int i = 0; i < numInRows; i++) {
+               array[0][i] = "*";
+            }
+
+            // This code places an X in the last row.
+            for (int i = 0; i < numInRows; i++) {
+                array[numInRows-1][i] = "*";
+            }
+
+            //This code places an X in the last column.
+            //[0,7],[1,7], [2,7]
+            for (int i = 0; i < numInRows; i++) {
+                array[i][numInRows-1] = "*";
+            }
+
+
+            // This code is for printing it all out
+            for (int i = 0; i < numInRows; i++) {
+                System.out.print(Arrays.toString(array[i]) + "\n");
+            }
             
             
         }
@@ -59,13 +94,13 @@ public class Shapes {
               
             //diagonal line
               for (int i = 0; i < numInRows; i++) {
-                 array[i][i] = "X";
+                 array[i][i] = "*";
                 }
             
             //opposite diagonal
 
             for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
-                array[i][k] = "X";
+                array[i][k] = "*";
             }
               
             // This code is for printing it all out
@@ -73,17 +108,62 @@ public class Shapes {
                 System.out.print(Arrays.toString(array[i]) + "\n");
             }
             
-
-
         }
         else if (unknownShape.equals(squareWithXShape)) {
             System.out.println("You have decided the Box with X Shape with " + numInRows + " rows and " + numInColumns + " columns");
 
+            String[][] array = new String[numInRows][numInColumns];
+
+            // blank slate
+            for (int i = 0; (i < numInRows); i++) {
+                for (int k = 0; k < numInColumns; k++) {
+                 array[i][k] = " ";
+               }
+            }
+              
+            //diagonal line
+            for (int i = 0; i < numInRows; i++) {
+                array[i][i] = "*";
+            }
             
+            //opposite diagonal
+            for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
+                array[i][k] = "*";
+            }
+
+            // This code will place an X at the first column.
+            for (int i = 0; i < numInRows; i++) {
+                array[i][0] = "*";
+            }
+
+            // This code places an X in the first row.
+            for (int i = 0; i < numInRows; i++) {
+               array[0][i] = "*";
+            }
+
+            // This code places an X in the last row.
+            for (int i = 0; i < numInRows; i++) {
+                array[numInRows-1][i] = "*";
+            }
+
+            //This code places an X in the last column.
+            //[0,7],[1,7], [2,7]
+            for (int i = 0; i < numInRows; i++) {
+                array[i][numInRows-1] = "*";
+            }
+              
+            // This code is for printing it all out
+            for (int i = 0; i < numInRows; i++) {
+                System.out.print(Arrays.toString(array[i]) + "\n");
+            }
+
 
         }
         else {
-           System.out.println("You have entered invalid input.");
+           System.out.println("You have entered invalid input.\n You must type exactly as it says in the options; Etc: \"Box\"\n");
+
+
+
         }
 
         
