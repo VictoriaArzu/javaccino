8861530ff7a2f3428c42868ee5928122f4724da1
Victoria Arzumanova <victoriaarzumanova123@gmail.com>
2020-10-16 15:20:40 -0400

    i have no idea as to what im doing. please do not come back to this god forbidden part of my code

--- Shapes/Shapes.java
+++ Shapes/Shapes.java
@@ -2,6 +2,7 @@ package Shapes;
 
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.*;
 
 public class Shapes {
     public static void main(String[] args) {
@@ -13,6 +14,10 @@ public class Shapes {
         String xShape = "X";
         String squareWithXShape = "Box with X";
         int numInRows, numInColumns;
+
+        Box.shape = new Box("Box");
+        X.shape = new X("X");
+
 
         //start program
         //desc of program
@@ -40,132 +45,140 @@ public class Shapes {
         }
 
         if (unknownShape.equals(boxShape)) {
-            System.out.println("You have decided the Box Shape with " + numInRows + " rows and " + numInColumns + " columns");
-
-            String[][] array = new String[numInRows][numInColumns];
-
-            // blank slate
-            for (int i = 0; (i < numInRows); i++) {
-                for (int k = 0; k < numInColumns; k++) {
-                 array[i][k] = " ";
-               }
-            }
-            
-            // This code will place an X at the first column.
-            for (int i = 0; i < numInRows; i++) {
-                array[i][0] = "*";
-            }
-
-            // This code places an X in the first row.
-            for (int i = 0; i < numInRows; i++) {
-               array[0][i] = "*";
-            }
-
-            // This code places an X in the last row.
-            for (int i = 0; i < numInRows; i++) {
-                array[numInRows-1][i] = "*";
-            }
-
-            //This code places an X in the last column.
-            //[0,7],[1,7], [2,7]
-            for (int i = 0; i < numInRows; i++) {
-                array[i][numInRows-1] = "*";
-            }
-
-
-            // This code is for printing it all out
-            for (int i = 0; i < numInRows; i++) {
-                System.out.print(Arrays.toString(array[i]) + "\n");
-            }
-            
             
         }
         else if (unknownShape.equals(xShape)) {
-            System.out.println("You have decided the X Shape with " + numInRows + " rows and " + numInColumns + " columns");
-
-            String[][] array = new String[numInRows][numInColumns];
-
-            // blank slate
-            for (int i = 0; (i < numInRows); i++) {
-                for (int k = 0; k < numInColumns; k++) {
-                 array[i][k] = " ";
-               }
-            }
-              
-            //diagonal line
-              for (int i = 0; i < numInRows; i++) {
-                 array[i][i] = "*";
-                }
-            
-            //opposite diagonal
-
-            for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
-                array[i][k] = "*";
-            }
-              
-            // This code is for printing it all out
-            for (int i = 0; i < numInRows; i++) {
-                System.out.print(Arrays.toString(array[i]) + "\n");
-            }
             
         }
         else if (unknownShape.equals(squareWithXShape)) {
-            System.out.println("You have decided the Box with X Shape with " + numInRows + " rows and " + numInColumns + " columns");
-
-            String[][] array = new String[numInRows][numInColumns];
-
-            // blank slate
-            for (int i = 0; (i < numInRows); i++) {
-                for (int k = 0; k < numInColumns; k++) {
-                 array[i][k] = " ";
-               }
-            }
-              
-            //diagonal line
-            for (int i = 0; i < numInRows; i++) {
-                array[i][i] = "*";
-            }
-            
-            //opposite diagonal
-            for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
-                array[i][k] = "*";
-            }
-
-            // This code will place an X at the first column.
-            for (int i = 0; i < numInRows; i++) {
-                array[i][0] = "*";
-            }
-
-            // This code places an X in the first row.
-            for (int i = 0; i < numInRows; i++) {
-               array[0][i] = "*";
-            }
-
-            // This code places an X in the last row.
-            for (int i = 0; i < numInRows; i++) {
-                array[numInRows-1][i] = "*";
-            }
-
-            //This code places an X in the last column.
-            //[0,7],[1,7], [2,7]
-            for (int i = 0; i < numInRows; i++) {
-                array[i][numInRows-1] = "*";
-            }
-              
-            // This code is for printing it all out
-            for (int i = 0; i < numInRows; i++) {
-                System.out.print(Arrays.toString(array[i]) + "\n");
-            }
-
-
+        
         }
         else {
            System.out.println("You have entered invalid input.\n You must type exactly as it says in the options; Etc: \"Box\"\n");
-
-
-
         }
-
         
     }
+
+    void Box(String shape){
+        System.out.println("You have decided the Box Shape with " + numInRows + " rows and " + numInColumns + " columns");
+
+        String[][] array = new String[numInRows][numInColumns];
+
+        // blank slate
+        for (int i = 0; (i < numInRows); i++) {
+            for (int k = 0; k < numInColumns; k++) {
+             array[i][k] = " ";
+           }
+        }
+        
+        // This code will place an X at the first column.
+        for (int i = 0; i < numInRows; i++) {
+            array[i][0] = "*";
+        }
+
+        // This code places an X in the first row.
+        for (int i = 0; i < numInRows; i++) {
+           array[0][i] = "*";
+        }
+
+        // This code places an X in the last row.
+        for (int i = 0; i < numInRows; i++) {
+            array[numInRows-1][i] = "*";
+        }
+
+        //This code places an X in the last column.
+        //[0,7],[1,7], [2,7]
+        for (int i = 0; i < numInRows; i++) {
+            array[i][numInRows-1] = "*";
+        }
+
+
+        // This code is for printing it all out
+        for (int i = 0; i < numInRows; i++) {
+            System.out.print(Arrays.toString(array[i]) + "\n");
+        }
+        
+        
+    }
+
+    void xShape(String shape) {
+        System.out.println("You have decided the X Shape with " + numInRows + " rows and " + numInColumns + " columns");
+
+        String[][] array = new String[numInRows][numInColumns];
+
+        // blank slate
+        for (int i = 0; (i < numInRows); i++) {
+            for (int k = 0; k < numInColumns; k++) {
+             array[i][k] = " ";
+           }
+        }
+          
+        //diagonal line
+          for (int i = 0; i < numInRows; i++) {
+             array[i][i] = "*";
+            }
+        
+        //opposite diagonal
+
+        for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
+            array[i][k] = "*";
+        }
+          
+        // This code is for printing it all out
+        for (int i = 0; i < numInRows; i++) {
+            System.out.print(Arrays.toString(array[i]) + "\n");
+        }
+        
+    }
+
+    void XwithBox(String shape){
+        System.out.println("You have decided the Box with X Shape with " + numInRows + " rows and " + numInColumns + " columns");
+
+        String[][] array = new String[numInRows][numInColumns];
+
+        // blank slate
+        for (int i = 0; (i < numInRows); i++) {
+            for (int k = 0; k < numInColumns; k++) {
+             array[i][k] = " ";
+           }
+        }
+          
+        //diagonal line
+        for (int i = 0; i < numInRows; i++) {
+            array[i][i] = "*";
+        }
+        
+        //opposite diagonal
+        for(int i = 0, k = numInRows - 1; i < numInRows ;i++, k--) {
+            array[i][k] = "*";
+        }
+
+        // This code will place an X at the first column.
+        for (int i = 0; i < numInRows; i++) {
+            array[i][0] = "*";
+        }
+
+        // This code places an X in the first row.
+        for (int i = 0; i < numInRows; i++) {
+           array[0][i] = "*";
+        }
+
+        // This code places an X in the last row.
+        for (int i = 0; i < numInRows; i++) {
+            array[numInRows-1][i] = "*";
+        }
+
+        //This code places an X in the last column.
+        //[0,7],[1,7], [2,7]
+        for (int i = 0; i < numInRows; i++) {
+            array[i][numInRows-1] = "*";
+        }
+          
+        // This code is for printing it all out
+        for (int i = 0; i < numInRows; i++) {
+            System.out.print(Arrays.toString(array[i]) + "\n");
+        }
+    }
 }
+
+
